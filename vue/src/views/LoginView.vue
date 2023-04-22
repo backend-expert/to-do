ef92c4d8b930afa7db2583aa7ddd9c4b2f4fd58d
Isaque Silva <isaque.silva@equilibriumweb.com>
2023-04-22 08:09:06 -0300

    falhou !

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -3,13 +3,33 @@
         <!-- <LoginMenu /> -->
         <h1>Login</h1>
 
+        <!-- <ValidationObserver
+            ref="loginForm"
+            tag="form"
+            @submit.stop.prevent="login()"
+        > -->
+
+        <form @submit.stop.prevent="login()" >
+
+
         <div>
             <label for="">Email</label> <br>
-            <input
-                v-model="email"
-                placeholder="Email"
-                type="email"
-            />
+
+            <!-- <ValidationProvider
+                v-slot="{errors}"
+
+                immediate
+                rules="secret"  
+                 
+            > -->
+         
+            <!-- <span>{{ errors[0] }}</span> -->
+                <input
+                    v-model="email"
+                    placeholder="Digite seu Email"
+                    type="text"
+                />
+            <!-- </ValidationProvider> -->
         </div>
 
         <div>
@@ -23,22 +43,35 @@
 
         <div>
             <button
-            @click.prevent="login()"
+          
+          type="submit" 
+                
+         
             >Entrar</button>
         </div>
+        </form>
+    
+        <!-- </ValidationObserver> -->
     </div>
 </template>
+
+
 <script>
 // import LoginMenu from '@/components/Auth/LoginMenu';
+
 import axios from '@/plugins/axios';
 import Cookie from 'js-cookie';
+// import { ValidationObserver, ValidationProvider,  extend  } from 'vee-validate';
 
 export default {
     name: 'Login',
 
     components: {
         // LoginMenu,
+   
     },
+
+    
 
     data() {
        // Defining the initial state of the component's data properties `email` and `password` as empty
@@ -50,7 +83,14 @@ export default {
     },
 
     methods: {
+        // async 
         login() {
+            // await this.$refs.loginForm.validate();
+            
+
+            // console.log('login');
+            // return ;
+
             const payload = {
                 email: this.email,
                 password: this.password
@@ -58,23 +98,13 @@ export default {
 
             //conexao ajax
             axios.post('/login', payload).then((response) => {
-
-                // `const token = `${response.data.token_type} ${response.data.access_token}`;` is
-                // creating a string that concatenates the `token_type` and `access_token` values from
-                // the `response.data` object returned by the server after a successful login request.
-                // This string represents the authentication token that will be used to authenticate
-                // subsequent requests to the server.
-                const token = `${response.data.token_type} ${response.data.access_token}`; 
+     const token = `${response.data.token_type} ${response.data.access_token}`; 
 
                 Cookie.set('_todolist_token', token, {
                     expires:30
                 });
 
-                // `this..commit('user/STORE_USER', response.data.data)` is committing a mutation
-                // called `STORE_USER` to the Vuex store's `user` module. The mutation is passing
-                // `response.data.data` as the payload, which will update the state of the `user`
-                // module with the new user data.
-                this.$store.commit('user/STORE_USER', response.data.data);
+                   this.$store.commit('user/STORE_USER', response.data.data);
             });
 
         },
